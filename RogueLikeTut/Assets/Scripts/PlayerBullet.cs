a58f2cef17addf659cf9c7fb7044fdfb892e3dc4
Ben Washburn <Benjamin@orkangraphics.com>
2020-02-24 20:49:35 -0500

    Player and Fire creation

---
+++ RogueLikeTut/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed = 7.5f;
+    public Rigidbody2D theRB;
+
+    public GameObject impactEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+        
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ RogueLikeTut/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 moveInput;
+
+    public Rigidbody2D theRB;
+    public Transform gunHand;
+
+    private Camera theCam;
+
+    public Animator anim;
+
+    public GameObject bulletToFire;
+    public Transform firePoint;
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+
+        //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
+        theRB.velocity = moveInput * moveSpeed;
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+        if(mousePos.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunHand.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            gunHand.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        //rotate gun arm
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x)* Mathf.Rad2Deg;
+
+        gunHand.rotation = Quaternion.Euler(0, 0, angle);
+
+
+        //Interact
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+            shotCounter = timeBetweenShots;
+        }
+
+        if( Input.GetMouseButton(0))
+        {
+            shotCounter -= Time.deltaTime;
+
+            if(shotCounter <= 0)
+            {
+                Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+                shotCounter = timeBetweenShots;
+            }
+        }
+
+
+
+
+
+
+
+        if(moveInput != Vector2.zero)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+
+
+    }
+}
