1d4ea9ad2c531c4082cb062cadd178bdcdcbb105
Ben Washburn <Benjamin@orkangraphics.com>
2020-02-25 21:03:28 -0500

    MakeSkelton
    Setup first enemy

---
+++ RogueLikeTut/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+
+    public float rangeToChasePlayer;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
