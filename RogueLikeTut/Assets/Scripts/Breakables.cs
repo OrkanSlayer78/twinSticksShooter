5b6380d82cff4061d6990d8c0d0159f6e98babe7
Ben Washburn <Benjamin@orkangraphics.com>
2020-04-21 22:50:28 -0400

    Health pickups randomly spawn from box destruction

--- RogueLikeTut/Assets/Scripts/Breakables.cs
+++ RogueLikeTut/Assets/Scripts/Breakables.cs
@@ -7,6 +7,10 @@ public class Breakables : MonoBehaviour
     public GameObject[] brokenPieces;
     public int maxPieces = 10;
 
+    public bool shouldDropItem;
+    public GameObject[] itemsToDrop;
+    public float itemDropPercent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,12 +31,27 @@ public class Breakables : MonoBehaviour
             if (PlayerController.instance.dashCounter > 0)
             {
                 Destroy(gameObject);
+                
+                //show debris
                 int piecesToDrop = Random.Range(1, maxPieces);
 
                 for(int i = 0; i<piecesToDrop; i++)
                 {
                     int randomPiece = Random.Range(0, brokenPieces.Length);
                     Instantiate(brokenPieces[randomPiece], transform.position, transform.rotation);
+                }
+
+                //random item drop
+                if (shouldDropItem)
+                {
+                    float dropChance = Random.Range(0f, 100f);
+                    if(dropChance < itemDropPercent)
+                    {
+                        int randomDrop = Random.Range(0, itemsToDrop.Length);
+
+                        Instantiate(itemsToDrop[randomDrop], transform.position, transform.rotation);
+
+                    }
                 }
                 
             }
