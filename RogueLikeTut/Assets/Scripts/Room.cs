937285570e676a5adc7fb3418be18a7643762385
Ben Washburn <Benjamin@orkangraphics.com>
2020-04-24 12:32:45 -0400

    Doors and door spawn on room change

--- RogueLikeTut/Assets/Scripts/Room.cs
+++ RogueLikeTut/Assets/Scripts/Room.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+
+    public bool closeWhenEntered;
+
+    public GameObject[] doors;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +26,14 @@ public class Room : MonoBehaviour
         if(other.tag == "Player")
         {
             CameraController.instance.ChangeTarget(transform);
+
+            if (closeWhenEntered)
+            {
+                foreach(GameObject door in doors)
+                {
+                    door.SetActive(true);
+                }
+            }
         }
     }
 }
