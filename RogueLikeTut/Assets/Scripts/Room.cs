19f4f96c8082afd3136e20f93b50fe4588e2096b
Ben Washburn <Benjamin@orkangraphics.com>
2020-04-24 13:36:06 -0400

    Door mechanics

--- RogueLikeTut/Assets/Scripts/Room.cs
+++ RogueLikeTut/Assets/Scripts/Room.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
 
-    public bool closeWhenEntered;
+    public bool closeWhenEntered, openWhenEnemiesCleared;
 
     public GameObject[] doors;
 
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private bool roomActive;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +21,26 @@ public class Room : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (enemies.Count > 0 && roomActive && openWhenEnemiesCleared)
+        {
+            for(int i = 0; i <enemies.Count; i++)
+            {
+                if(enemies[i] == null)
+                {
+                    enemies.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            if(enemies.Count == 0)
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.SetActive(false);
+                    closeWhenEntered = false;
+                }
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -34,6 +56,13 @@ public class Room : MonoBehaviour
                     door.SetActive(true);
                 }
             }
+
+            roomActive = true;
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        roomActive = false;
     }
 }
