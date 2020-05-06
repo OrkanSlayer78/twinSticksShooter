1ba372b44e3b7d0b64691bf7b60085410ee989e6
Ben Washburn <Benjamin@orkangraphics.com>
2020-05-06 14:11:10 -0400

    level generation room abstraction

--- RogueLikeTut/Assets/Scripts/Room.cs
+++ RogueLikeTut/Assets/Scripts/Room.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
 
-    public bool closeWhenEntered, openWhenEnemiesCleared;
+    public bool closeWhenEntered;
 
     public GameObject[] doors;
 
-    public List<GameObject> enemies = new List<GameObject>();
-
-    private bool roomActive;
+    
+    [HideInInspector]
+    public bool roomActive;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,28 +21,17 @@ public class Room : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (enemies.Count > 0 && roomActive && openWhenEnemiesCleared)
-        {
-            for(int i = 0; i <enemies.Count; i++)
-            {
-                if(enemies[i] == null)
-                {
-                    enemies.RemoveAt(i);
-                    i--;
-                }
-            }
-
-            if(enemies.Count == 0)
-            {
-                foreach (GameObject door in doors)
-                {
-                    door.SetActive(false);
-                    closeWhenEntered = false;
-                }
-            }
-        }
+        
     }
 
+    public void openDoors()
+    {
+        foreach (GameObject door in doors)
+        {
+            door.SetActive(false);
+            closeWhenEntered = false;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
